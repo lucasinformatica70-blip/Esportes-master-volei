987ad7650156c1d543ce89393227c65f524082b3
lucasinformatica70-blip <lucasinformatica70@gmail.com>
2026-05-19 15:39:00 -0300

    mudar arquivos de html para php

---
+++ esportes - master/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta author="Lucas, Amanda e Rafaela">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esportes - Vôlei</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/script.js" defer></script>
+</head>
+<body>
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li><img src="./img/logo.png" alt="Logo" height="60px" width="120px"></li>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="./sobre.php">Sobre</a></li>
+                    <li><a href="./modalidades.php">Modalidades</a></li>
+                    <li><a href="./contato.php">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <h1>Esportes Master</h1>
+    <h3>Vôlei</h3>
+    <div class="slideshow-container">
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 7</div>
+    <img src="./img/img1.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 7</div>
+    <img src="./img/img2.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 7</div>
+    <img src="./img/img3.jpg" style="width:100%">
+  </div>
+
+<div class="mySlides fade">
+    <div class="numbertext">4 / 7</div>
+    <img src="./img/img4.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">5 / 7</div>
+    <img src="./img/img5.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">6 / 7</div>
+    <img src="./img/img6.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">7 / 7</div>
+    <img src="./img/img7.jpg" style="width:100%">
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+</div>
+    <br>
+    <footer>
+        <p>Direitos Reservados a Lucas V. de Freitas, Amanda Beatriz fir e Rafaela Luiza N. Cavalcante</p>
+        <p>cnpj: 11.111.111/0001-11</p>
+        <p>Endereço: Rua dos Esportes, 123 - Cidade Esportiva, MG, Brasil</p>
+        <p>CEP: 35000-000</p>
+    </footer>
+</body>
+</html>
