9d0d4811386c44e66b9517f2a6034242bde09336
lucasinformatica70-blip <lucasinformatica70@gmail.com>
2026-05-19 16:26:00 -0300

    site

--- esportes - master/contato.php
+++ esportes - master/contato.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
+
+<php lang="pt-br">
+
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contato</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <div class="contato">
     Nome Completo:
 <input type="text" name="nome" placeholder="Digite seu nome completo"><br><br>
 E-mail: <input type="email" name="email" placeholder="Digite seu e-mail"><br><br>
@@ -13,6 +19,14 @@ Assunto: <input type="text" name="assunto" placeholder="Digite o assunto da mens
 Mensagem: <textarea name="mensagem" placeholder="Digite sua mensagem aqui"></textarea><br><br>
 <input type="submit" value="Enviar">        
 </div>
+
+
+<ul>
+    <li>
+        <a href="index.php"></a>
+    </li>
+</ul>
+
 <footer>
     <p>CNPJ</p>
     <p>Endereço: Rua 1 - Bairro: Horto Florestal - Belo Horizonte | Minas Gerais | Brasil</p>
@@ -25,6 +39,7 @@ Whatsapp</a></li>
     <li><a href="#">Facebook</a></li>
 </li>
 </ul>
+</div>
 
 
 <p align=center><a > Lucas,Amanda,Rafaela - 2026</a></p>
--- esportes - master/index.php
+++ esportes - master/index.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<!DOCTYPE php>
+<php lang="pt-br">
 <head>
-    <meta author="Lucas, Amanda e Rafaela">
+    <meta author="Lucas V. de Freitas, Amanda Beatriz fiereck e Rafaela Luiza N. Cavalcante">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esportes - Vôlei</title>
     <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/script.js" defer></script>
 </head>
 <body>
     <header>
@@ -13,17 +14,68 @@
             <nav>
                 <ul>
                     <li><img src="./img/logo.png" alt="Logo" height="60px" width="120px"></li>
-                    <li><a href="./index.html">Home</a></li>
-                    <li><a href="./sobre.html">Sobre</a></li>
-                    <li><a href="./contato.html">Contato</a></li>
-                    <li><a href="./modalidades.html">Modalidades</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="sobre.php">Sobre</a></li>
+                    <li><a href="modalidades.php">Modalidades</a></li>
+                    <li><a href="contato.php">Contato</a></li>
                 </ul>
             </nav>
         </div>
     </header>
-    <h1>Vôlei</h1>
-    <p>Adicionar texto/substituir por imagem</p>
-    
+
+  <!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 7</div>
+    <img src="./img/img1.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 7</div>
+    <img src="./img/img2.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 7</div>
+    <img src="./img/img3.jpg" style="width:100%">
+  </div>
+    <div class="mySlides fade">
+    <div class="numbertext">4 / 7</div>
+    <img src="./img/img4.jpg" style="width:100%">
+  </div>
+    <div class="mySlides fade">
+    <div class="numbertext">5 / 7</div>
+    <img src="./img/img5.jpg" style="width:100%">
+  </div>
+    <div class="mySlides fade">
+    <div class="numbertext">6 / 7</div>
+    <img src="./img/img6.jpg" style="width:100%">
+  </div>
+    <div class="mySlides fade">
+    <div class="numbertext">7 / 7</div>
+    <img src="./img/img7.jpg" style="width:100%">
+  </div>
+
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+  <span class="dot" onclick="currentSlide(6)"></span>
+  <span class="dot" onclick="currentSlide(7)"></span>
+</div>
+
     <br>
     <footer>
         <p>Direitos Reservados a Lucas V. de Freitas, Amanda Beatriz fir e Rafaela Luiza N. Cavalcante</p>
@@ -32,4 +84,6 @@
         <p>CEP: 35000-000</p>
     </footer>
 </body>
+</php>
 </html>
+
